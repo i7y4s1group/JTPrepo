59cf97734c9ec60db65b85355b0934c7aa9567f8
thekosa2 <michal.kosieradzki@post.pl>
2019-05-09 20:38:35 +0200

    rozgrzebanie lab4

--- lab4/src/main/java/pl/lab4/zadanie/Product.java
+++ lab4/src/main/java/pl/lab4/zadanie/Product.java
@@ -1,20 +1,41 @@
 package pl.lab4.zadanie;
 
+import javafx.util.Builder;
+
 public class Product {
     private String productName;
     private int productPrice;
     private int productMargin;
 
-    public void setProductMargin(int productMargin) {
-        this.productMargin = productMargin;
+    public static class Builder {
+        private String productName;
+        private int productPrice;
+        private int productMargin;
+
+        public Builder productName(String productName) {
+            this.productName = productName;
+            return this;
+        }
+
+        public Builder productPrice(int productPrice) {
+            this.productPrice = productPrice;
+            return this;
+        }
+
+        public Builder productMargin(int productMargin) {
+            this.productMargin = productMargin;
+            return this;
+        }
+
+        public Product build() {
+            return new Product(this);
+        }
     }
 
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public void setProductPrice(int productPrice) {
-        this.productPrice = productPrice;
+    private Product(Builder builder) {
+        this.productName = builder.productName;
+        this.productPrice = builder.productPrice;
+        this.productMargin = builder.productMargin;
     }
 
     public int getProductMargin() {
